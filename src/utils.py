eb8ab60dac035b84b98cdf71bda7a3894b324177
Gaurav Kumar Chaurasiya <2002gauravkumarchaurasiya@gmail.com>
2024-01-21 22:49:29 +0530

    Hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from src.exception import CustomException
 from src.logger import logging
+from sklearn.model_selection import GridSearchCV
 import dill
 from sklearn.metrics import r2_score
 def save_object(file_path,obj):
@@ -18,21 +19,23 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            # para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            # gs = GridSearchCV(model,para,cv=3)
-            # gs.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            # model.set_params(**gs.best_params_)
-            # model.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
+            logging.info(f"Best Parameters: {gs.best_params_}")
 
-            model.fit(X_train, y_train)  # Train model
+            model.fit(X_train,y_train)
+            logging.info("Done Hyperparameter tuning")
+            # model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
